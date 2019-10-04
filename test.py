2fa3e6a6a1cda1afe5a957ce4f37f5fa30f8baa2
raysaavedra-work <raysaavedra.work@gmail.com>
2019-10-04 02:36:40 +0000

    Updated tests for assessment

--- test.py
+++ test.py
@@ -202,7 +202,7 @@ class AssessmentTestCases(unittest.TestCase):
         return res.group().split("'")[1]
 
     def _get_url(self):
-        return "https://sheltered-journey-48409.herokuapp.com/"
+        return "https://rocky-oasis-46830.herokuapp.com/"
 
     def _check_webhook_data(self):
         x = urllib.request.urlopen(f'{self._get_url()}/payment_intent')
